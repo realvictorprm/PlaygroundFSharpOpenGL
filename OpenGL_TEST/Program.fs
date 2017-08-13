c24bc4b6e5612f89119978067cb2318b6ac8c4b9
realvictorprm <mueller.vpr@gmail.com>
2017-08-13 17:39:51 +0200

    Initial commit containing the scaffold for doing OpenGL stuff with F#!

---
+++ OpenGL_TEST/Program.fs
@@ -0,0 +1,52 @@
+﻿// Weitere Informationen zu F# unter "http://fsharp.org".
+// Weitere Hilfe finden Sie im Projekt "F#-Tutorial".
+open OpenTK
+open OpenTK.Graphics.OpenGL4
+open System
+
+let startGame () =
+    use window = new GameWindow(400, 400, Graphics.GraphicsMode.Default, "F# OpenGL Test", GameWindowFlags.Default)
+    window.Visible <- true
+    let glContext = window.Context
+    
+    let loadAndCompileShader (shaderType:ShaderType) path =
+        let content = System.IO.File.ReadAllText path
+        let shader = GL.CreateShader(shaderType)
+        do GL.ShaderSource(shader, content)
+        do GL.CompileShader shader
+        shader
+
+    let createFragmentShader path = loadAndCompileShader ShaderType.FragmentShader path
+    let createVertexShader path = loadAndCompileShader ShaderType.VertexShader path
+    
+    let createShaderProgram shaders =
+        let program = GL.CreateProgram()
+        shaders |> List.iter(fun shader -> GL.AttachShader(program, shader))
+        do GL.LinkProgram(program)
+        program
+    
+    let vertShader, fragShader = 
+        createVertexShader @"../../../shaders/FirstChapter/SimpleLight.vert",
+        createFragmentShader @"../../../shaders/FirstChapter/SimpleLight.frag"
+    
+    let shaderProgram = createShaderProgram [vertShader; fragShader]
+    let shader = { Utils.Shader.ID = shaderProgram }
+
+    let model = Utils.Model.create "../../../models/nanosuit/nanosuit.obj"
+
+    let rec gameLoop dt =
+        let measure = dt
+        let timeout = System.DateTime.Now.AddMilliseconds(16.).Millisecond
+        model.Meshes |> Seq.tryHead |> Option.iter (fun mesh -> Utils.Mesh.draw mesh shader)
+
+        System.Threading.Thread.Sleep(timeout - System.DateTime.Now.Millisecond)
+        gameLoop measure
+    gameLoop 0.016
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    printfn "%A" (System.IO.Directory.GetCurrentDirectory())
+    startGame ()
+    System.Console.ReadKey() |> ignore
+    0 // Integer-Exitcode zurückgeben
---
+++ OpenGL_TEST/Utils.fs
@@ -0,0 +1,91 @@
+﻿module Utils
+open System.Runtime.InteropServices
+
+
+let getModel path =
+    use context = new Assimp.AssimpContext()
+    let scene = context.ImportFile path
+    scene.Meshes
+
+type Shader = {
+    ID : int
+}
+
+[<Struct>]
+type Vertex = {
+    Position:OpenTK.Vector3
+    Normal:OpenTK.Vector3
+}
+
+module Vertex =
+    let offset_Position = Marshal.OffsetOf(typeof<Vertex>, "Position")
+    let offset_Normal = Marshal.OffsetOf(typeof<Vertex>, "Normal")
+    let size = Marshal.SizeOf<Vertex>()
+
+[<Struct>]
+type Mesh = {
+    Vertices : Vertex[]
+    Indices : int []
+    VAO : int
+    VBO : int
+    EBO : int
+}
+
+module Mesh =
+    open OpenTK
+    open OpenTK.Graphics.OpenGL4
+    open OpenTK.Graphics.OpenGL4
+    open System
+    open System.Runtime.InteropServices
+
+    let create (vertices:Vertex []) (indices:int []) =
+        let vao, vbo, ebo = GL.GenVertexArray(), GL.GenBuffer(), GL.GenBuffer()
+        do GL.BindVertexArray vao
+        do GL.BindBuffer(BufferTarget.ArrayBuffer, vbo)
+        do GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length, vertices, BufferUsageHint.StaticDraw)
+
+        do GL.BindBuffer(BufferTarget.ArrayBuffer, ebo)
+        do GL.BufferData(BufferTarget.ArrayBuffer, indices.Length, indices, BufferUsageHint.StaticDraw)
+
+        do GL.EnableVertexAttribArray(0)
+        do GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, Vertex.size, Vertex.offset_Position)
+        do GL.EnableVertexAttribArray(1)
+        do GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, Vertex.size, Vertex.offset_Normal)
+
+        do GL.BindVertexArray(0)
+
+        {   Vertices = vertices
+            Indices = indices
+            VAO = vao
+            VBO = vbo
+            EBO = ebo   }
+    
+    let createOpenTKVector3 (vec3:Assimp.Vector3D) = OpenTK.Vector3(vec3.X, vec3.Y, vec3.Z)
+    
+    let createFromAssimpMesh (mesh:Assimp.Mesh) =
+        let vertices = 
+            [|   for i in 0..(mesh.VertexCount - 1) do
+                    let pos = createOpenTKVector3 mesh.Vertices.[i]
+                    let normal = createOpenTKVector3 mesh.Normals.[i]
+                    yield { Position =  pos; Normal = normal} |]
+        let indicies = mesh.GetIndices()
+        create vertices indicies
+    
+    let draw (mesh:Mesh) (shader:Shader) = 
+        do GL.BindVertexArray(mesh.VAO)
+        do GL.UseProgram shader.ID
+        do GL.DrawElements(BeginMode.Triangles, mesh.Indices.Length, DrawElementsType.UnsignedInt, 0)
+        do GL.BindVertexArray(0)
+
+type Model = {
+    Meshes : Mesh seq
+}
+
+module Model = 
+    open OpenTK
+    open OpenTK.Graphics.OpenGL4
+    
+    let create path =
+        let absPath = System.IO.Path.GetFullPath path
+        let model = getModel absPath
+        { Meshes = model |> Seq.map (fun mesh -> Mesh.createFromAssimpMesh mesh) }
